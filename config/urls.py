07367283d25845427808b610825204bce393eb0e
amirhossein <amirhossein.rahimi494@gmail.com>
2022-11-01 11:39:47 +0330

    add customers urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('doctor/',include('doctors.urls'))
+    path('doctor/',include('doctors.urls')),
+    path('customer/',include('customers.urls'))
 ]

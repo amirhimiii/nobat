b43fc7710622549ba7b26362da04317b75cee0c0
amirhossein <amirhossein.rahimi494@gmail.com>
2022-11-01 11:46:34 +0330

    views and urls  for doctors

--- doctors/urls.py
+++ doctors/urls.py
@@ -3,7 +3,19 @@ from .views import *
 
 from allauth.account.views import * 
 
-urlpatterns = [
+
+
+
+urlpatterns= [
+    path('', doctor_list, name='doctor-list'),
+    path('detail/<slug:slug>/', doctor_view, name='doctor-detail'),
+]
+
+
+
+
+
+urlpatterns += [
     path('signup/', doctor_signup,name='account_signup'),
     # path('customer/',customer_signup,name= 'customer_signup')
     path("login/", login, name="account_login"),
@@ -47,38 +59,3 @@ urlpatterns = [
     ),
 ]
 
-# urlpatterns+= [
-#     # path("signup/", signup, name="account_signup"),
-#     path("login/", login, name="account_login"),
-#     path("password/set/", password_set, name="account_set_password"),
-#     path("inactive/", account_inactive, name="account_inactive"),
-#     # E-mail
-#     path("email/", email, name="account_email"),
-#     path(
-#         "confirm-email/",
-#         email_verification_sent,
-#         name="account_email_verification_sent",
-#     ),
-#     re_path(
-#         r"^confirm-email/(?P<key>[-:\w]+)/$",
-#         confirm_email,
-#         name="account_confirm_email",
-#     ),
-#     # password reset
-#     path("password/reset/", password_reset, name="account_reset_password"),
-#     path(
-#         "password/reset/done/",
-#         password_reset_done,
-#         name="account_reset_password_done",
-#     ),
-#     re_path(
-#         r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
-#         password_reset_from_key,
-#         name="account_reset_password_from_key",
-#     ),
-#     path(
-#         "password/reset/key/done/",
-#         password_reset_from_key_done,
-#         name="account_reset_password_from_key_done",
-#     ),
-# ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,16 +1,58 @@
 from django.shortcuts import render
+from .models import Doctor , Information
+
+#allauth
 from .forms import DoctorSignupForm
 from allauth.account.views import SignupView
+
+#rest
+from .models import Information
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from .serializers import CustomUserSerializer,CustomUserDetail
+from .permissions import UserPermissions, UserUpdatePermissions
 
 
 
 
 
 class DoctorView(SignupView):
+    """
+    allauth signup form for doctors
+    """
+
     template_name = 'account/signup.html'
     form_class = DoctorSignupForm
     view_name = 'doctor_signup'
     
 
-
 doctor_signup = DoctorView.as_view()
+
+
+# tarif view hay rest (retrieve , update, list, destroy)
+
+
+
+class DoctorListView(generics.ListAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [UserPermissions]
+    
+doctor_list = DoctorListView.as_view()
+
+
+
+class DoctorRetriveUpdateViews(generics.RetrieveUpdateAPIView):
+    serializer_class = CustomUserDetail 
+    permission_classes = [UserUpdatePermissions]
+
+    def get_object(self):
+        user = self.request.user
+        slug = self.kwargs.get('slug')
+        queryset = Information.objects.select_related('user').get(user__slug=slug)
+        return queryset
+
+
+
+doctor_view = DoctorRetriveUpdateViews.as_view()

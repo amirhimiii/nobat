4f690f6c8068bfb6194210a94ce311cc54931cf9
amirhossein <amirhossein.rahimi494@gmail.com>
2022-11-01 11:44:22 +0330

    admin inline for doctors

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Doctor
+from .models import Doctor, Information
 from accounts.models import User
 # Register your models here.
-admin.site.register(Doctor)
+# admin.site.register(Doctor)
 admin.site.register(User)
+
+
+
+
+class InformationInline(admin.StackedInline):
+    model = Information
+    fields =['address','image','shift','sec_images','about','active']
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    inlines = [InformationInline]
+    list_display = ['first_name','last_name','licenses','email']
+    # actions = [make_deactive_product,make_active_product]
+    list_filter = ('licenses',)
+    search_fields = ('licenses','last_name')
